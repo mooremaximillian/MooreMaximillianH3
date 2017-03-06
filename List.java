57bde95c93a694a2e9806df393e1b943d3255299
Maximillian Moore <mooremaximillian@gmail.com>
2017-03-05 19:16:47 -0500

    Creates properites and documents the list and main classes

---
+++ List.java
@@ -0,0 +1,49 @@
+/**
+ * List Class to make methods to operate on the doubly linked list
+ **/ 
+public class List
+{
+  // intializes the Node head
+ private Node head;
+ // initalizes the Node tail
+ private Node tail;
+ 
+ 
+ public List()
+   
+ {
+   
+ }
+ 
+ public List(Node head, Node tail)
+ {
+   
+ }
+ 
+ public void addNode(String node)
+ {
+   
+ }
+ 
+ public void alphabeticalOrder(String name)
+ {
+   
+   
+ }
+ 
+ public void findNode(String name)
+ {
+   
+ }
+ 
+ public void deleteNode(String name)
+ {
+   
+ }
+ 
+ public void deleteList(Node node)
+ {
+   
+ }
+  
+}
